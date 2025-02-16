981fccdeb44766ba5550d083feb149536f579bfa
PARATOS855 <paratos855@gmail.com>
2025-02-17 01:46:14 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7556448904:AAHXoaWY0D2l1lWlfiOPXP2JvZEnXjYTDj4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("EXTENDER_YT")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1204807401")
+
+
+
+
+
+
+
+  
